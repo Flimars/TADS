05edc065652db62291863fcb5eed25d0243ae161
Flávio Lima <flavio.lima@aluno.riogrande.ifrs.edu.br>
2024-03-17 08:38:44 -0300

    Concluído Trbl02 - todos exercícios feitos.

--- Desenvolvimento de Aplicativos Web I/flima/Trbl02/ex09.php
+++ Desenvolvimento de Aplicativos Web I/flima/Trbl02/ex09.php
@@ -8,4 +8,31 @@
     • SONHO SONHO SONHO SONHO
 */
 
+$palavra = "SONHO";
+$contador = 0;
+
+do {
+    switch ($contador) {
+        case 0:
+            echo "$palavra\n";
+            break;
+        case 1:
+            echo "$palavra $palavra\n";
+            break;
+        case 2:
+            echo "$palavra $palavra $palavra\n";
+            break;
+        case 3:
+            echo "$palavra $palavra $palavra $palavra\n";
+            break;
+        
+        default:
+            echo "Valor Inválido";
+            break;
+    }
+    $contador++;
+    
+} while ($contador <= 3);
+
+
 ?>
